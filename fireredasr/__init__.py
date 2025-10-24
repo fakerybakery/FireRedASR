8b4189928ccc09e96bc2fc60e181a6734c07fd4e
mrfakename <me@mrfake.name>
2025-10-23 20:34:56 -0700

    turn into a package

---
+++ fireredasr/__init__.py
@@ -0,0 +1,7 @@
+"""FireRedASR: Open-Source Industrial-Grade Automatic Speech Recognition Models"""
+
+__version__ = "0.1.0"
+
+from fireredasr.models.fireredasr import FireRedAsr
+
+__all__ = ["FireRedAsr"]
---
+++ fireredasr/models/module/__init__.py
@@ -0,0 +1,1 @@
+"""Model submodules for FireRedASR"""
